35a020ecfd0185c640b00d488c93bbd11bb187bb
alpha-yu <1184129467@qq.com>
2023-07-15 18:39:30 +0800

    module_login初版

--- src/main/java/module_login/module_login.java
+++ src/main/java/module_login/module_login.java
@@ -1,12 +1,14 @@
 package module_login;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -15,83 +17,94 @@ import module_shared.shared;
 import javax.swing.*;
 import java.sql.DriverManager;
 
-public class module_login extends Application{
+public class module_login extends Application {
     static Stage stage = new Stage();
-    static TextField user = new TextField("sa");
-    static TextField password = new TextField("123456");
-    static TextField DB = new TextField("Java");
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        showLogin();
-    }
+    static TextField user = new TextField();
+    static TextField psw = new TextField();
+    static ComboBox<String> role=new ComboBox<>();
 
     public static void showLogin() {
         stage.setTitle("Login");
-        Button btOK = new Button("OK");
-        Button btCancel = new Button("Cancel");
-        Font font = new Font("Times New Roman", 13);
-
+        Font font = new Font("Times New Roman", 18);
+        Button btLogin = new Button("Log In");
+        Button btSignup = new Button("Sign Up");
         GridPane pane = new GridPane();
+
+        pane.setHgap(5);
         pane.setAlignment(Pos.CENTER);
-        pane.setVgap(8);
-        GridPane setPane = new GridPane();
 
-        btOK.setPrefWidth(70);
-        btCancel.setPrefWidth(70);
-        setPane.setAlignment(Pos.CENTER);
-        setPane.setHgap(10);
-        setPane.add(btOK, 0, 0);
-        setPane.add(btCancel, 1, 0);
+        btLogin.setPrefWidth(500);
+        btSignup.setPrefWidth(500);
+        btLogin.setPrefHeight(40);
+        btSignup.setPrefHeight(40);
+        btLogin.setFont(new Font("黑体", 20));
+        btSignup.setFont(new Font("黑体", 20));
 
-        user.setFont(font);
-        user.setPrefWidth(100);
-        password.setFont(font);
-        password.setPrefWidth(100);
-        DB.setFont(font);
-        DB.setPrefWidth(100);
-        DB.setEditable(true);
+        GridPane titlePane = new GridPane();
+        Label titleLabel = new Label("SHOP MANAGERMENT");
+        titleLabel.setFont(new Font("黑体", 45));
+        titlePane.setAlignment(Pos.CENTER);
+        titlePane.add(titleLabel,0,0);
+
         GridPane UserPane = new GridPane();
-        GridPane PasswordPane = new GridPane();
-        GridPane DBPane = new GridPane();
+        Label userLabel = new Label("User Name");
+        userLabel.setFont(font);
+        user.setFont(font);
+        user.setPrefWidth(500);
+        UserPane.add(userLabel, 0, 0);
+        UserPane.add(user, 0, 1);
+        UserPane.setVgap(5);
 
-        Label UserLabel = new Label("User:");
-        UserLabel.setPrefWidth(80);
-        UserPane.add(UserLabel, 0, 0);
-        UserPane.add(user, 1, 0);
-        Label PasswordLabel = new Label("Password:");
-        PasswordLabel.setPrefWidth(80);
-        PasswordPane.add(PasswordLabel, 0, 0);
-        PasswordPane.add(password, 1, 0);
-        Label DBLabel = new Label("Database:");
-        DBLabel.setPrefWidth(80);
-        DBPane.add(DBLabel, 0, 0);
-        DBPane.add(DB, 1, 0);
-        pane.add(UserPane, 0, 0);
-        pane.add(PasswordPane, 0, 1);
-        pane.add(DBPane, 0, 2);
-        pane.add(setPane, 0, 3);
+        GridPane pswPane = new GridPane();
+        Label pswLabel = new Label("Password");
+        pswLabel.setFont(font);
+        psw.setFont(font);
+        psw.setPrefWidth(500);
+        pswPane.add(pswLabel, 0, 0);
+        pswPane.add(psw, 0, 1);
+        pswPane.setVgap(5);
 
-        btOK.setOnMouseClicked(e -> {
-            loginExecute();
-        });
+        GridPane RolePane =new GridPane();
+        Label roleLabel =new Label("Role");
+        ObservableList<String> options = FXCollections.observableArrayList("Option 1","Option 2","Option 3");
+        role.setItems(options);
+        roleLabel.setFont(font);
+        role.setPrefWidth(500);
+        RolePane.add(roleLabel, 0, 0);
+        RolePane.add(role, 0, 1);
+        RolePane.setVgap(5);
 
-        btOK.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.ENTER) {
-                loginExecute();
-            }
-        });
 
-        btCancel.setOnMouseClicked(e -> {
-            System.exit(0);
-        });
 
-        Scene scene = new Scene(pane, 220, 160);
-        scene.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.ENTER) {
-                loginExecute();
-            }
-        });
+
+//        btLogin.setOnMouseClicked(e -> {
+//            loginExecute();
+//        });
+//
+//        btLogin.setOnKeyPressed(e -> {
+//            if (e.getCode() == KeyCode.ENTER) {
+//                loginExecute();
+//            }
+//        });
+//
+//        btSignup.setOnMouseClicked(e -> {
+//            System.exit(0);
+//        });
+
+        pane.setVgap(15);
+        pane.setAlignment(Pos.CENTER);
+        pane.add(titlePane, 0, 0);
+        pane.add(UserPane, 0, 1);
+        pane.add(pswPane, 0, 2);
+        pane.add(RolePane, 0, 3);
+        pane.add(btLogin, 0, 4);
+        pane.add(btSignup, 0, 5);
+        Scene scene = new Scene(pane, 800, 600);
+//        scene.setOnKeyPressed(e -> {
+//            if (e.getCode() == KeyCode.ENTER) {
+//                loginExecute();
+//            }
+//        });
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
@@ -100,8 +113,8 @@ public class module_login extends Application{
     public static void loginExecute() {
         try {
             shared.userStr = user.getText();
-            shared.passwordStr = password.getText();
-            String DBStr = DB.getText();
+            shared.passwordStr = psw.getText();
+            String DBStr = "Shop";
             String DBTemp = shared.dbURL + DBStr;
 
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -118,7 +131,8 @@ public class module_login extends Application{
         }
     }
 
-//    public static void main(String[] args){
-//        launch();
-//    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        showLogin();
+    }
 }
