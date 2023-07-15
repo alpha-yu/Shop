a6d99e5102f79fb673f471db9fe30e86f47a9236
3389889639 <90179149+3389889639@users.noreply.github.com>
2023-07-15 18:04:01 +0800

    Update User.java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,6 +3,30 @@ public class User {
     private String _password;
     private int auth;
 
+    public String get_userName() {
+        return _userName;
+    }
+
+    public void set_userName(String _userName) {
+        this._userName = _userName;
+    }
+
+    public String get_password() {
+        return _password;
+    }
+
+    public void set_password(String _password) {
+        this._password = _password;
+    }
+
+    public int getAuth() {
+        return auth;
+    }
+
+    public void setAuth(int auth) {
+        this.auth = auth;
+    }
+
     public void ShowMenu()
     {
         
