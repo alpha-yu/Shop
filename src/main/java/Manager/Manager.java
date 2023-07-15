8e89abe3d5d31f30e759ae9072af6804d8e1785c
3389889639 <90179149+3389889639@users.noreply.github.com>
2023-07-15 21:20:28 +0800

    Update Manager.java

--- src/main/java/Manager/Manager.java
+++ src/main/java/Manager/Manager.java
@@ -1,6 +1,18 @@
 package Manager;
 
 import module_shared.User;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 public class Manager extends User
 {
@@ -8,5 +20,10 @@ public class Manager extends User
     public  void ShowMenu()
     {
 
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ShowMenu();
     }
 }
