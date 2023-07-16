9a2a49bdfccfb5dce8f4f464ea5df0f51f521291
alpha-yu <1184129467@qq.com>
2023-07-16 11:57:56 +0800

    update

--- 数据库创建.sql
+++ 数据库创建.sql
@@ -1,4 +1,4 @@
-if exists(select * from sys.databases where name='Shop') 
+if exists(select * from sys.databases where name='Shop')
 begin
     drop database Shop;
 end
@@ -40,9 +40,9 @@ Inprice dec(10,4),
 Infee   dec(10,4),
 constraint PK_SG primary key(Sno,Gno),
 constraint FK_SG_S foreign key (Sno) references Supplier(Sno)
-	on update cascade,
+    on update cascade,
 constraint FK_SG_G foreign key (Gno) references Good(Gno)
-	on update cascade
+    on update cascade
 )
 
 create table Purchase(
@@ -53,13 +53,14 @@ Sno     varchar(20),
 Ptime   datetime,
 Pnum    int,
 Pprice  dec(10,4),
-Pperson varchar(30)
+Pperson varchar(30),
+Pflag	int,
 constraint FK_P_G foreign key (Gno) references Good(Gno)
-	on update cascade,
+    on update cascade,
 constraint FK_P_S foreign key (Sno) references Supplier(Sno)
-	on update cascade,
+    on update cascade,
 constraint FK_P_U foreign key (Pperson) references Users(username)
-	on update cascade
+    on update cascade
 )
 
 create table Orders(
@@ -72,16 +73,16 @@ Otime  datetime,
 Oprice dec(10,4),
 Oinfo  int,
 constraint FK_O_U foreign key (Obuyer) references Users(username)
-	on update cascade,
+    on update cascade,
 constraint FK_O_G foreign key (Gno) references Good(Gno)
-	on update cascade
+    on update cascade
 )
 
 create table Store(
 Pno  varchar(20) primary key,
 Snum int,
 constraint FK_S_O foreign key (Pno) references Purchase(Pno)
-	on update cascade
+    on update cascade
 )
 
 create table Discount(
@@ -95,22 +96,22 @@ Gno5  varchar(20),
 PCT   dec(10,4),
 TPR   dec(10,4),
 constraint FK_D_S1 foreign key (Gno1) references Good(Gno)
-	on update no action,
+    on update no action,
 constraint FK_D_S2 foreign key (Gno2) references Good(Gno)
-	on update no action,
+    on update no action,
 constraint FK_D_S3 foreign key (Gno3) references Good(Gno)
-	on update no action,
+    on update no action,
 constraint FK_D_S4 foreign key (Gno4) references Good(Gno)
-	on update no action,
+    on update no action,
 constraint FK_D_S5 foreign key (Gno5) references Good(Gno)
-	on update no action
+    on update no action
 )
 
 go
 create view V_Sell as
-	select G.Gno,G.Gname,G.Gprice,sum(S.Snum) as Ssum
-	from Store S,Purchase P,Good G
-	where S.Pno=P.Pno and P.Gno=G.Gno
-	group by G.Gno,G.Gname,G.Gprice
-
+    select G.Gno,G.Gname,G.Gprice,sum(S.Snum) as Ssum
+    from Store S,Purchase P,Good G
+    where S.Pno=P.Pno and P.Gno=G.Gno
+    group by G.Gno,G.Gname,G.Gprice
+ go
 select * from V_Sell
