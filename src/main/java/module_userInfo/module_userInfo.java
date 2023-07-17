fec20e9ecba5c48e634f0c94f2e45b16ea31e609
AiyangBuyang <630691850@qq.com>
2023-07-17 23:54:20 +0800

    Update module_userInfo.java

--- src/main/java/module_userInfo/module_userInfo.java
+++ src/main/java/module_userInfo/module_userInfo.java
@@ -9,11 +9,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import module_main.module_main;
 import module_shared.shared;
 
+import java.awt.geom.Area;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import static module_shared.shared.button_change;
+import static module_shared.shared.init_Button_Style;
 
 public class module_userInfo extends Application {
     static Stage stage;
@@ -22,10 +27,86 @@ public class module_userInfo extends Application {
         launch(args);
     }
 
+    public static void createStage(MyText text, method m, TableView tableView, String title, String option){
+        Stage stage = new Stage();
+        stage.setTitle(title);
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);//居中
+        pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+        pane.setHgap(10);
+        pane.setVgap(10);
+
+        Label l1 = new Label("用户姓名：");
+        l1.setStyle("-fx-font-size: 15px;");
+        TextField v1 = new TextField();
+        v1.setText(text.getUsername());
+        Label l2 = new Label("用户密码：");
+        l2.setStyle("-fx-font-size: 15px;");
+        TextField v2 = new TextField();
+        v2.setText(text.getPsw());
+        Label l3 = new Label("用户权限：");
+        l3.setStyle("-fx-font-size: 15px;");
+        TextField v3 = new TextField();
+        String x = text.getAUTH() + "";
+        v3.setText(x);
+        Label l4 = new Label("联系方式：");
+        l4.setStyle("-fx-font-size: 15px;");
+        TextField v4 = new TextField();
+        v4.setText(text.getUcif());
+        Label l5 = new Label("     地址：");
+        l5.setStyle("-fx-font-size: 15px;");
+        TextField v5 = new TextField();
+        v5.setText(text.getUaddr());
+        Button sure = new Button("确认");
+        init_Button_Style(sure, 30, 60);
+        button_change(sure);
+        pane.add(l1, 0, 0);
+        pane.add(v1, 1, 0);
+        pane.add(l2, 0, 1);
+        pane.add(v2, 1, 1);
+        pane.add(l3, 0, 2);
+        pane.add(v3, 1, 2);
+        pane.add(l4, 0, 3);
+        pane.add(v4, 1, 3);
+        pane.add(l5, 0, 4);
+        pane.add(v5, 1, 4);
+        pane.add(sure, 1, 6);
+        GridPane.setHalignment(sure, HPos.RIGHT);//右对齐
+
+        //显示
+        Scene scene2 = new Scene(pane, 400, 300);
+        stage.setScene(scene2);
+        stage.setResizable(false);
+        stage.show();
+
+        sure.setOnAction(b -> {
+            if(option.equals("alter")){
+                String username = v1.getText();
+                String psw = v2.getText();
+                String auth = v3.getText();
+                String ucif = v4.getText();
+                String Uaddr = v5.getText();
+                int AUTH = Integer.parseInt(auth);
+                m.update(text.getUsername(), username, psw, AUTH, ucif, Uaddr);//修改操作
+            } else if (option.equals("add")) {
+                String username = v1.getText();
+                String psw = v2.getText();
+                String auth = v3.getText();
+                String ucif = v4.getText();
+                String Uaddr = v5.getText();
+                int AUTH = Integer.parseInt(auth);
+                m.add(username, psw, AUTH, ucif, Uaddr);//修改操作
+            }
+            stage.close();//关闭当前界面
+            m.sel();
+            tableView.setItems(FXCollections.observableList(m.orders));
+            tableView.refresh();
+        });
+    }
     public static void showUserInfo(){
         stage=new Stage();
         method m=new method();
-        m.sel("Users");
+        m.sel();
         stage.setTitle("用户管理");
         //创建页面标题
         Label titleLabel = new Label("用户管理");
@@ -36,7 +117,7 @@ public class module_userInfo extends Application {
         searchField.setPromptText("请输入用户名");
         Button searchButton = new Button("搜索");
         //设置‘搜索’按钮颜色
-        shared.init_Button_Style(searchButton,20,60);
+        init_Button_Style(searchButton,20,60);
         shared.button_change(searchButton);
         //使文本框于搜索按钮并排
         HBox hBox = new HBox(10, searchField, searchButton);
@@ -60,47 +141,45 @@ public class module_userInfo extends Application {
         // 设置表格数据
         tableView.setItems(FXCollections.observableList(m.orders));
 
-        Button b1 = new Button("增加");
-        Button b2 = new Button("删除");
-        Button b3 = new Button("修改");
-        Button b4 = new Button("刷新");
-        shared.init_Button_Style(b1,30,60);
-        shared.init_Button_Style(b2,30,60);
-        shared.init_Button_Style(b3,30,60);
-        shared.init_Button_Style(b4,30,60);
-        shared.button_change(b1);
-        shared.button_change(b2);
-        shared.button_change(b3);
-        shared.button_change(b4);
+        Button buttonAdd = new Button("增加");
+        Button buttonDelete = new Button("删除");
+        Button buttonAlter = new Button("修改");
+        init_Button_Style(buttonAdd,30,60);
+        init_Button_Style(buttonDelete,30,60);
+        init_Button_Style(buttonAlter,30,60);
+        shared.button_change(buttonAdd);
+        shared.button_change(buttonDelete);
+        shared.button_change(buttonAlter);;
         //合并按钮
-        FlowPane f1 = new FlowPane(b1, b2, b3, b4);
-        f1.setHgap(20);//水平间距
+        FlowPane f1 = new FlowPane(buttonAdd, buttonDelete, buttonAlter);
+        f1.setHgap(50);//水平间距
         f1.setPadding(shared.menuPadding);//内间距
 
         //按用户名查询
         searchButton.setOnAction(a -> {
             String username = searchField.getText();
-            m.select(username);
+            if (username.isEmpty()) {
+                tableView.setItems(FXCollections.observableList(m.orders));
+            } else {
+                List<MyText> searchResult = new ArrayList<>();
+                for (MyText myText : m.orders) {
+                    if (myText.getUsername().contains(username)) {
+                        searchResult.add(myText);
+                    }
+                }
+                tableView.setItems(FXCollections.observableList(searchResult));
+            }
+        });
 
-            // 设置表格数据
-            tableView.setItems(FXCollections.observableList(m.orders));
-            BorderPane borderPane = new BorderPane();
-            VBox vBox = new VBox();
-            vBox.setSpacing(10);
-            vBox.setPadding(new Insets(10));
-            vBox.getChildren().addAll(titleLabel, hBox, tableView);
-            borderPane.setCenter(vBox);
-            borderPane.setBottom(f1);
-            // 创建场景
-            Scene scene = new Scene(borderPane, 800, 600);
-            // 设置场景并显示
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.show();
+        //增加事件
+        buttonAdd.setOnAction(a -> {
+            MyText text = new MyText();
+            //创建新界面
+            createStage(text, m, tableView, "修改用户信息", "add");
         });
 
         //删除事件
-        b2.setOnAction(a -> {
+        buttonDelete.setOnAction(a -> {
             // 获取被选中的行
             MyText text = tableView.getSelectionModel().getSelectedItem();
             if (text == null) {
@@ -113,28 +192,14 @@ public class module_userInfo extends Application {
             if (btn.get() == ButtonType.OK) {
                 //删除
                 m.del(text.getUsername());
-                // 设置表格数据
-                method conn = new method();
-                conn.sel("Users");
-                tableView.setItems(FXCollections.observableList(conn.orders));
-                BorderPane borderPane = new BorderPane();
-                VBox vBox = new VBox();
-                vBox.setSpacing(10);
-                vBox.setPadding(new Insets(10));
-                vBox.getChildren().addAll(titleLabel, hBox, tableView);
-                borderPane.setCenter(vBox);
-                borderPane.setBottom(f1);
-                // 创建场景
-                Scene scene = new Scene(borderPane, 800, 600);
-                // 设置场景并显示
-                stage.setScene(scene);
-                stage.setResizable(false);
-                stage.show();
+                m.sel();
+                tableView.setItems(FXCollections.observableList(m.orders));
+                tableView.refresh();
             }
         });
 
         //修改事件
-        b3.setOnAction(a -> {
+        buttonAlter.setOnAction(a -> {
             // 获取被选中的行
             MyText text = tableView.getSelectionModel().getSelectedItem();
             if (text == null) {
@@ -142,177 +207,7 @@ public class module_userInfo extends Application {
                 return;
             }
             //创建新界面
-            Stage stage = new Stage();
-            stage.setTitle("增加学生");
-            GridPane pane = new GridPane();
-            pane.setAlignment(Pos.CENTER);//居中
-            pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
-            pane.setHgap(5.5);
-            pane.setVgap(5.5);
-
-            BorderPane borderPane2 = new BorderPane();
-            Label l1 = new Label("用户姓名：");
-            TextField v1 = new TextField();
-            v1.setText(text.getUsername());
-            Label l2 = new Label("用户密码：");
-            TextField v2 = new TextField();
-            v2.setText(text.getPsw());
-            Label l3 = new Label("用户权限：");
-            TextField v3 = new TextField();
-            String x = text.getAUTH() + "";
-            v3.setText(x);
-            Label l4 = new Label("联系方式：");
-            TextField v4 = new TextField();
-            v4.setText(text.getUcif());
-            Label l5 = new Label("     地址：");
-            TextField v5 = new TextField();
-            v5.setText(text.getUaddr());
-            Button sure = new Button("确认");
-            pane.add(l1, 0, 0);
-            pane.add(v1, 1, 0);
-            pane.add(l2, 0, 1);
-            pane.add(v2, 1, 1);
-            pane.add(l3, 0, 2);
-            pane.add(v3, 1, 2);
-            pane.add(l4, 0, 3);
-            pane.add(v4, 1, 3);
-            pane.add(l5, 0, 4);
-            pane.add(v5, 1, 4);
-            pane.add(sure, 1, 5);
-            GridPane.setHalignment(sure, HPos.RIGHT);//右对齐
-
-            //显示
-            Scene scene2 = new Scene(pane);
-            stage.setScene(scene2);
-            stage.setResizable(false);
-            stage.show();
-
-            sure.setOnAction(b -> {
-                //获取值
-                String username = v1.getText();
-                String psw = v2.getText();
-                String auth = v3.getText();
-                String ucif = v4.getText();
-                String Uaddr = v5.getText();
-                int AUTH = Integer.parseInt(auth);
-                m.update(text.getUsername(), username, psw, AUTH, ucif, Uaddr);//修改操作
-                stage.close();//关闭当前界面
-
-                // 设置表格数据
-                method conn = new method();
-                conn.sel("Users");
-                tableView.setItems(FXCollections.observableList(conn.orders));
-                BorderPane borderPane = new BorderPane();
-                VBox vBox = new VBox();
-                vBox.setSpacing(10);
-                vBox.setPadding(new Insets(10));
-                vBox.getChildren().addAll(titleLabel, hBox, tableView);
-                borderPane.setCenter(vBox);
-                borderPane.setBottom(f1);
-                // 创建场景
-                Scene scene = new Scene(borderPane, 800, 600);
-                // 设置场景并显示
-                stage.setScene(scene);
-                stage.setResizable(false);
-                stage.show();
-            });
-        });
-
-        //增加事件
-        b1.setOnAction(a -> {
-            //创建新界面
-            Stage stage = new Stage();
-            stage.setTitle("增加学生");
-            GridPane pane = new GridPane();
-            pane.setAlignment(Pos.CENTER);//居中
-            pane.setPadding(shared.menuPadding);
-            pane.setHgap(5.5);
-            pane.setVgap(5.5);
-
-            BorderPane borderPane2 = new BorderPane();
-            Label l1 = new Label("用户姓名：");
-            TextField v1 = new TextField();
-
-            Label l2 = new Label("用户密码：");
-            TextField v2 = new TextField();
-
-            Label l3 = new Label("用户权限：");
-            TextField v3 = new TextField();
-
-            Label l4 = new Label("联系方式：");
-            TextField v4 = new TextField();
-
-            Label l5 = new Label("地址：");
-            TextField v5 = new TextField();
-
-            Button sure = new Button("确认");
-            pane.add(l1, 0, 0);
-            pane.add(v1, 1, 0);
-            pane.add(l2, 0, 1);
-            pane.add(v2, 1, 1);
-            pane.add(l3, 0, 2);
-            pane.add(v3, 1, 2);
-            pane.add(l4, 0, 3);
-            pane.add(v4, 1, 3);
-            pane.add(l5, 0, 4);
-            pane.add(v5, 1, 4);
-            pane.add(sure, 1, 5);
-            GridPane.setHalignment(sure, HPos.RIGHT);//右对齐
-
-            //显示
-            Scene scene2 = new Scene(pane);
-            stage.setScene(scene2);
-            stage.setResizable(false);
-            stage.show();
-
-            sure.setOnAction(b -> {
-                //获取值
-                String username = v1.getText();
-                String psw = v2.getText();
-                String auth = v3.getText();
-                String ucif = v4.getText();
-                String Uaddr = v5.getText();
-                int AUTH = Integer.parseInt(auth);
-                m.add(username, psw, AUTH, ucif, Uaddr);//修改操作
-                stage.close();//关闭当前界面
-
-                // 设置表格数据
-                method conn = new method();
-                conn.sel("Users");
-                tableView.setItems(FXCollections.observableList(conn.orders));
-                BorderPane borderPane = new BorderPane();
-                VBox vBox = new VBox();
-                vBox.setSpacing(10);
-                vBox.setPadding(new Insets(10));
-                vBox.getChildren().addAll(titleLabel, hBox, tableView);
-                borderPane.setCenter(vBox);
-                borderPane.setBottom(f1);
-                // 创建场景
-                Scene scene = new Scene(borderPane, 800, 600);
-                // 设置场景并显示
-                stage.setScene(scene);
-                stage.setResizable(false);
-                stage.show();
-            });
-        });
-
-        //刷新界面
-        b4.setOnAction(a -> {
-            m.sel("Users");
-            tableView.setItems(FXCollections.observableList(m.orders));
-            BorderPane borderPane = new BorderPane();
-            VBox vBox = new VBox();
-            vBox.setSpacing(10);
-            vBox.setPadding(new Insets(10));
-            vBox.getChildren().addAll(titleLabel, hBox, tableView);
-            borderPane.setCenter(vBox);
-            borderPane.setBottom(f1);
-            // 创建场景
-            Scene scene = new Scene(borderPane, 800, 600);
-            // 设置场景并显示
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.show();
+            createStage(text, m, tableView, "修改用户信息", "alter");
         });
 
         // 创建布局
@@ -331,7 +226,6 @@ public class module_userInfo extends Application {
         stage.show();
 
     }
-
     @Override
     public void start(Stage stage) {
         showUserInfo();
