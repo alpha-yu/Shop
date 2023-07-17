710e26db7fef3f29f0aec496404ebcf4e4da5d9c
AiyangBuyang <630691850@qq.com>
2023-07-18 00:28:16 +0800

    Update shared.java

--- src/main/java/module_shared/shared.java
+++ src/main/java/module_shared/shared.java
@@ -47,7 +47,7 @@ public class shared {
     public static String dbURL = "jdbc:sqlserver://localhost:9615;DatabaseName=Shop";
     public static Connection dbConn = null;
     public static String userStr = "sa";
-    public static String passwordStr = "CHEN";
+    public static String passwordStr = "123456";
 
     //按钮风格初始化
     public static void init_Button_Style(Button bt, double h, double w) {
