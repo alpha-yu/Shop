0b50e11ea07385a68742717518075401f9ab0373
3389889639 <90179149+3389889639@users.noreply.github.com>
2023-07-17 10:49:00 +0800

    Update shared.java

--- src/main/java/module_shared/shared.java
+++ src/main/java/module_shared/shared.java
@@ -36,7 +36,7 @@ public class shared {
     public static String passwordStr = "123456";
 
     //按钮风格初始化
-    public static void init_Button_Style(Button bt, double h, double w) {
+    public void init_Button_Style(Button bt, double h, double w) {
         bt.setPrefHeight(h);
         bt.setPrefWidth(w);
         bt.setStyle(blue_background + white_text + shared.round);
